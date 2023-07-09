8574831a463aa74cfa88fd224ae2a0d4ae2d12d4
Isaiah A <isaiah.althouse@gmail.com>
2023-07-08 18:22:13 -0700

    Deserialize nested custom chips
    Adds `deserializeChips` method to ChipDeserializer that returns mapped object
    containing constructors for each chip.
    
    Allows for chips to be defined with "dependencies" on other custom chips,
    eg an `OR` gate constructed using a `NAND` gate.

--- src/__tests__/logic/chipSerializer.test.ts
+++ src/__tests__/logic/chipSerializer.test.ts
@@ -1,5 +1,76 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import {describe, expect, test} from 'vitest';
-import {type ChipDefinitionObject, deserializeChip} from '../../logic/chipSerializer';
+import {type ChipDefinitionObject, ChipDeserializer} from '../../logic/chipSerializer';
+
+type TruthTable = Array<Array<boolean | number>>;
+
+interface ChipExample {
+	table: TruthTable;
+	def: ChipDefinitionObject;
+}
+
+const chipExamples: Record<string, ChipExample> = {
+	nand: {
+		table: [
+			[0, 0, 1],
+			[0, 1, 1],
+			[1, 0, 1],
+			[1, 1, 0],
+		],
+		def: {
+			io: [2, 1],
+			chips: {
+				a: 'AND',
+				n: 'NOT',
+			},
+			edges: [
+				['IN', 0, 'a', 0],
+				['IN', 1, 'a', 1],
+				['a', 0, 'n', 0],
+				['n', 0, 'OUT', 0],
+			],
+		},
+	},
+	swap: {
+		table: [
+			[0, 0, 0, 0],
+			[0, 1, 1, 0],
+			[1, 0, 0, 1],
+			[1, 1, 1, 1],
+		],
+		def: {
+			io: [2, 2],
+			chips: {},
+			edges: [
+				['IN', 0, 'OUT', 1],
+				['IN', 1, 'OUT', 0],
+			],
+		},
+	},
+	or: {
+		table: [
+			[0, 0, 0],
+			[0, 1, 1],
+			[1, 0, 1],
+			[1, 1, 1],
+		],
+		def: {
+			io: [2, 1],
+			chips: {
+				n1: 'NOT',
+				n2: 'NOT',
+				nand: 'NAND',
+			},
+			edges: [
+				['IN', 0, 'n1', 0],
+				['IN', 1, 'n2', 0],
+				['n1', 0, 'nand', 0],
+				['n2', 0, 'nand', 1],
+				['nand', 0, 'OUT', 0],
+			],
+		},
+	},
+};
 
 describe('Chip de-serialization', () => {
 	describe('deserializes with correct number of inputs/outputs', () => {
@@ -16,67 +87,59 @@ describe('Chip de-serialization', () => {
 				edges: [],
 			} satisfies ChipDefinitionObject;
 
-			const chip = deserializeChip(definitionObj);
+			const chip = new ChipDeserializer().deserializeChip(definitionObj);
 			expect(chip.inputs.length).toBe(resIn);
 			expect(chip.outputs.length).toBe(resOut);
 		});
 	});
 
 	describe('deserializes custom "swap" circuit', () => {
-		const truthTable = [
-			[false, false, false, false],
-			[false, true, true, false],
-			[true, false, false, true],
-			[true, true, true, true],
-		];
+		test.each(chipExamples.swap.table)(
+			'[%s %s] -> [%s %s]',
+			(in1, in2, out1, out2) => {
+				const deserializer = new ChipDeserializer();
+				const chip = deserializer.deserializeChip(chipExamples.swap.def);
 
-		const swapCircuitDefinitionObj = {
-			io: [2, 2],
-			chips: {},
-			edges: [
-				['IN', 0, 'OUT', 1],
-				['IN', 1, 'OUT', 0],
-			],
-		} satisfies ChipDefinitionObject;
-
-		test.each(truthTable)('[%s %s] -> [%s %s]', (in1, in2, out1, out2) => {
-			const chip = deserializeChip(swapCircuitDefinitionObj);
-			chip.setInput(0, in1);
-			chip.setInput(1, in2);
-			expect(chip.outputs[0].state).toBe(out1);
-			expect(chip.outputs[1].state).toBe(out2);
-		});
+				chip.setInput(0, Boolean(in1));
+				chip.setInput(1, Boolean(in2));
+				expect(chip.outputs[0].state).toBe(Boolean(out1));
+				expect(chip.outputs[1].state).toBe(Boolean(out2));
+			});
 	});
 
 	describe('deserializes NAND circuit', () => {
-		const truthTable = [
-			// In1   In2    Output
-			[false, false, true],
-			[false, true, true],
-			[true, false, true],
-			[true, true, false],
-		];
+		test.each(chipExamples.nand.table)(
+			'%d ^& %d -> %d',
+			(in1, in2, res) => {
+				const deserializer = new ChipDeserializer();
+				const nand = deserializer.deserializeChip(chipExamples.nand.def);
 
-		const defObj = {
-			io: [2, 1],
-			chips: {
-				a: 'AND',
-				n: 'NOT',
+				nand.setInput(0, Boolean(in1));
+				nand.setInput(1, Boolean(in2));
+				const output = nand.outputs[0].state;
+
+				expect(output).toBe(Boolean(res));
+			});
+	});
+
+	describe('handles chip definitions containing custom chips', () => {
+		test.each(chipExamples.or.table)(
+			'%d || %d -> %d',
+			(in1, in2, res) => {
+				const deserializer = new ChipDeserializer();
+				const {OR} = deserializer.deserializeChips({
+					OR: chipExamples.or.def,
+					NAND: chipExamples.nand.def,
+				});
+
+				const or = new OR();
+
+				or.setInput(0, Boolean(in1));
+				or.setInput(1, Boolean(in2));
+				const output = or.outputs[0].state;
+
+				expect(output).toBe(Boolean(res));
 			},
-			edges: [
-				['IN', 0, 'a', 0],
-				['IN', 1, 'a', 1],
-				['a', 0, 'n', 0],
-				['n', 0, 'OUT', 0],
-			],
-		} satisfies ChipDefinitionObject;
-
-		test.each(truthTable)('%d ^& %d -> %d', (in1, in2, res) => {
-			const nand = deserializeChip(defObj);
-			nand.setInput(0, in1);
-			nand.setInput(1, in2);
-			const output = nand.outputs[0].state;
-			expect(output).toBe(res);
-		});
+		);
 	});
 });
--- src/logic/chipSerializer.ts
+++ src/logic/chipSerializer.ts
@@ -3,63 +3,123 @@
 import {AndGate} from './AndGate';
 import {type Chip} from './Chip';
 import {ComplexChip} from './ComplexChip';
+import {type Gate} from './Gate';
 import {NotGate} from './NotGate';
 import {type RelayGate} from './RelayGate';
 
-const gateLib = {
+type PureChipConstructor = new () => Gate | ComplexChip;
+
+export interface ChipDefinitionObject {
+	io: [number, number];
+	chips: Record<string, string>;
+	edges: Array<[string, number, string, number]>;
+}
+
+const GateLib = {
 	AND: AndGate,
 	NOT: NotGate,
 };
 
-export interface ChipDefinitionObject {
-	io: [number, number];
-	chips: Record<string, keyof typeof gateLib>;
-	edges: Array<[string, number, string, number]>;
+function mapObject<V, VR>(
+	obj: Record<string, V>,
+	mapFn: (entry: [k: string, v: V]) => [string, VR],
+): Record<string, VR> {
+	return Object.fromEntries(Object.entries<V>(obj)
+		.map(mapFn));
 }
 
-// TODO: Enable chip definitions to contain custom chips
-// - Given a list of chip definitions which reference each other,
-//   sort the definitions topologically based on their dependencies
-//   (eg, ChipB references ChipA, so ChipA must be deserialized first)
-// - Dynamically extend gateLib to include custom chips as they are defined
-export function deserializeChip(
-	def: ChipDefinitionObject,
-) {
-	return new ComplexChip(
-		def.io[0],
-		def.io[1],
-		(_in, _out) => {
-			chipGenerator(def, _in, _out);
-		},
-	);
-}
+export class ChipDeserializer {
+	#chipLib: Record<string, PureChipConstructor>;
 
-function chipGenerator(
-	def: ChipDefinitionObject,
-	IN: RelayGate,
-	OUT: RelayGate): void {
-	const chips: Record<string, Chip> = {
-		IN,
-		OUT,
-		...Object.fromEntries(Object.keys(def.chips)
-			.map(id => {
-				const ChipConstructor = gateLib[def.chips[id]];
+	constructor(chipLib = {}) {
+		this.#chipLib = chipLib;
+	}
+
+	deserializeChips<K extends string>(
+		defs: Record<K, ChipDefinitionObject>,
+	): Record<K, PureChipConstructor> {
+		// 1. Get list of chips
+		// NOTE: Currently, because constructors are not run at "compile" time,
+		// NOTE: there is no sorting based on dependencies (ie, if the definition of OR requires chipLib.NAND).
+		// NOTE: If this were to change, you would want to Topologically Sort chipNames
+		// NOTE: before generating the constructors.
+		const chipNames = Object.keys(defs) as K[];
+
+		// 2. Process each def in order,
+		//    adding the chip constructor to #chipLib if it's not already there.
+		return Object.fromEntries(chipNames.map(k => {
+			const Constructor = this.#chipFactory(defs[k]);
+
+			if (!(k in this.#chipLib)) {
+				this.#chipLib[k] = Constructor;
+			}
+
+			return [k, Constructor];
+		})) as Record<K, PureChipConstructor>;
+	}
+
+	deserializeChip(
+		def: ChipDefinitionObject,
+	) {
+		const ChipConstructor = this.#chipFactory(def);
+		return new ChipConstructor();
+	}
+
+	#chipFactory(def: ChipDefinitionObject) {
+		const chipGenerator = (_in: RelayGate, _out: RelayGate) => {
+			this.#chipGenerator(def, _in, _out);
+		};
+
+		return class GeneratedChip extends ComplexChip {
+			constructor() {
+				super(
+					def.io[0],
+					def.io[1],
+					chipGenerator,
+				);
+			}
+		};
+	}
+
+	#chipGenerator(
+		def: ChipDefinitionObject,
+		IN: RelayGate,
+		OUT: RelayGate): void {
+		const chips: Record<string, Chip> = {
+			IN,
+			OUT,
+			...mapObject(def.chips, ([id]) => {
+				let ChipConstructor;
+
+				if (def.chips[id] in GateLib) {
+					const constructorId = def.chips[id] as keyof typeof GateLib;
+					ChipConstructor = GateLib[constructorId];
+				} else if (def.chips[id] in this.#chipLib) {
+					const constructorId = def.chips[id];
+					ChipConstructor = this.#chipLib[constructorId];
+				}
+
+				if (!ChipConstructor) {
+					throw new Error(`Invalid chip name ${id}: ${def.chips[id]}`);
+				}
+
 				return [id, new ChipConstructor()];
-			})),
-	};
+			}),
+		};
 
-	def.edges.forEach(([inId, inPin, outId, outPin]) => {
-		if (!chips[inId]) {
-			throw new Error(`Could not find chip ${inId}`);
-		}
+		def.edges.forEach(([inId, inPin, outId, outPin]) => {
+			if (!chips[inId]) {
+				throw new Error(`Could not find chip ${inId}`);
+			}
 
-		if (!chips[outId]) {
-			throw new Error(`Could not find chip ${outId}`);
-		}
+			if (!chips[outId]) {
+				throw new Error(`Could not find chip ${outId}`);
+			}
 
-		const inChip = chips[inId];
-		const outChip = chips[outId];
+			const inChip = chips[inId];
+			const outChip = chips[outId];
 
-		inChip.addListener(inPin, {chip: outChip, pin: outPin});
-	});
+			inChip.addListener(inPin, {chip: outChip, pin: outPin});
+		});
+	}
 }
